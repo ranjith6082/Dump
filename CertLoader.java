73611c0be233387ccdb43ac5de49da6e793f375e
ranjith krishna <145783382+ranjith6082@users.noreply.github.com>
2026-09-01 19:34:57 +0530

    Update CertLoader.java

--- CertLoader.java
+++ CertLoader.java
@@ -1,9 +1,12 @@
 {
-  "errorMessage": "software.amazon.awssdk.services.s3.model.S3Exception: null (Service: S3, Status Code: 403, Request ID: null, Extended Request ID: +OEuExDuT2sThh7yp1dcW5vwflsJcsa8Q0eb0G1Wca3K03XjB7PBBYha71Px3DU0eE3q2FDk28U=)",
-  "errorType": "java.lang.RuntimeException",
-  "stackTrace": [
-    "com.adcb.cert.S3FileLambda.handleRequest(S3FileLambda.java:77)",
-    "java.base/jdk.internal.reflect.DirectMethodHandleAccessor.invoke(Unknown Source)",
-    "java.base/java.lang.reflect.Method.invoke(Unknown Source)"
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": "arn:aws:s3:::YOUR-BUCKET-NAME/*"
+    }
   ]
 }
