72d53c087b7e9e3cd010dee94dcd04efc3e4aec9
ranjith krishna <145783382+ranjith6082@users.noreply.github.com>
2026-09-02 10:52:11 +0530

    Update CertLoader.java

--- CertLoader.java
+++ CertLoader.java
@@ -1,12 +1,48 @@
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Resource": "arn:aws:s3:::YOUR-BUCKET-NAME/*"
-    }
-  ]
-}
+Please verify this event bridge approach, 
+ 
+we will verify the secret manager version changes instead of expiry date in the source code to read the values from Secret Manager.
+Use Event bridge to reload the configuration from Lambda in warm state
+Cost is negligible only as it will occur only once for every 2 years. 
+ 
+Secrets Manager
+
+      |
+
+      | Certificate Updated
+
+      v
+
+EventBridge Rule
+
+      |
+
+      v
+
+Refresh Controller Lambda
+
+      |
+
+      +----------------------------+
+
+      |                            |
+
+      v                            v
+
+Update Lambda A             Update Lambda B
+
+      |                            |
+
+      +------------+---------------+
+
+                   |
+
+                   v
+
+New Execution Environments
+
+                   |
+
+                   v
+
+Read New Certificate
+ 
